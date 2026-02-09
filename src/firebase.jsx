0f049de1504090233b40c973d21532125e79f71d
Sherwindias10 <sherwindias56@gmail.com>
2026-02-09 16:28:41 +0530

    Initialize Firebase configuration and services

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD_8YiPfXkZggJQOZGjxiPwMHSzWSH-pOQ",
+  authDomain: "erp-saas-platform.firebaseapp.com",
+  projectId: "erp-saas-platform",
+  storageBucket: "erp-saas-platform.firebasestorage.app",
+  messagingSenderId: "232433018921",
+  appId: "1:232433018921:web:99de012385c65522af1438"
+  measurementId: "G-QSLX22N53H"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
